4b0e0d862d72d326daa5c2e94d0b4621a7f96355
Biraj Rajbhandari <birajr0@gmail.com>
2022-01-19 13:32:46 +0545

    Adds validation to the form

--- src/Add.jsx
+++ src/Add.jsx
@@ -4,10 +4,18 @@ import styles from "./Add.module.css";
 
 function Add({ setTodoItems }) {
   const [item, setItem] = useState("");
+  const [err, setErr] = useState(false);
   const handleSubmit = (e) => {
     {
       e.preventDefault();
-      setTodoItems((prev) => [...prev, item]);
+      setErr(false);
+      if (item === "") {
+        setErr(true);
+      }
+      if (item) {
+        setTodoItems((prev) => [...prev, item]);
+        setItem("");
+      }
     }
   };
   return (
@@ -19,6 +27,7 @@ function Add({ setTodoItems }) {
           onChange={(e) => setItem(e.target.value)}
           width={15}
           className={styles.input}
+          error={err}
         />
         <Button icon type="submit" size="large">
           <Icon name="add" />
